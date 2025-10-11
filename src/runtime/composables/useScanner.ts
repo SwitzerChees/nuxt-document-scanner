a5cd7d47fdc9346070c5040f55a665e7cf881822
switzerchees <patrickmichel96@yahoo.de>
2025-10-11 23:21:20 +0200

    Add performance monitoring to useScanner for enhanced profiling
    - Introduced a performance monitor to track and log the average time taken for key operations: getFrame, inferCorners, postprocessImage, and total processing time.
    - Implemented logic to reset performance data after logging every 50 values to manage memory usage.
    - Enhanced the scanner loop to include performance measurement, improving the ability to analyze and optimize scanning efficiency.

--- src/runtime/composables/useScanner.ts
+++ src/runtime/composables/useScanner.ts
@@ -66,6 +66,42 @@ export function useScanner(opts: DocumentScannerOptions) {
     stopStream()
   }
 
+  const performanceMonitor: {
+    getFrame: {
+      avg: number
+      values: number[]
+    }
+    inferCorners: {
+      avg: number
+      values: number[]
+    }
+    postprocessImage: {
+      avg: number
+      values: number[]
+    }
+    total: {
+      avg: number
+      values: number[]
+    }
+  } = {
+    getFrame: {
+      avg: 0,
+      values: [],
+    },
+    inferCorners: {
+      avg: 0,
+      values: [],
+    },
+    postprocessImage: {
+      avg: 0,
+      values: [],
+    },
+    total: {
+      avg: 0,
+      values: [],
+    },
+  }
+
   const scannerLoop = async () => {
     if (!video.value || !overlay.value || !isStarted.value) return
 
@@ -77,13 +113,20 @@ export function useScanner(opts: DocumentScannerOptions) {
       await restartStream()
     }
 
+    const getFrameStart = performance.now()
     const videoFrame = getFrame()
     if (!videoFrame) return requestAnimationFrame(scannerLoop)
+    performanceMonitor.getFrame.values.push(performance.now() - getFrameStart)
 
     // Yield briefly to let browser render before heavy work
     await new Promise((r) => setTimeout(r, 0))
 
+    const inferCornersStart = performance.now()
     await inferCorners(videoFrame)
+    performanceMonitor.inferCorners.values.push(
+      performance.now() - inferCornersStart,
+    )
+
     updateProgress(isStable.value)
 
     if (canAutoCapture()) {
@@ -95,13 +138,48 @@ export function useScanner(opts: DocumentScannerOptions) {
       captureRequested.value = false
       const finalFrame = getFrame()
       await inferCorners(copyImageData(finalFrame!))
+      const postprocessImageStart = performance.now()
       const page = postprocessImage(finalFrame!, currentCorners.value!)
       if (page) currentDocument.value?.pages.push(page)
+      performanceMonitor.postprocessImage.values.push(
+        performance.now() - postprocessImageStart,
+      )
     }
+    performanceMonitor.total.values.push(performance.now() - frameStart)
+    performanceMonitor.total.avg =
+      performanceMonitor.total.values.reduce((a, b) => a + b, 0) /
+      performanceMonitor.total.values.length
+    performanceMonitor.getFrame.avg =
+      performanceMonitor.getFrame.values.reduce((a, b) => a + b, 0) /
+      performanceMonitor.getFrame.values.length
+    performanceMonitor.inferCorners.avg =
+      performanceMonitor.inferCorners.values.reduce((a, b) => a + b, 0) /
+      performanceMonitor.inferCorners.values.length
+    performanceMonitor.postprocessImage.avg =
+      performanceMonitor.postprocessImage.values.reduce((a, b) => a + b, 0) /
+      performanceMonitor.postprocessImage.values.length
 
     // Keep frame timing consistent, avoid blocking next frame
     const elapsed = performance.now() - frameStart
     const delay = Math.max(0, frameDuration - elapsed)
+
+    // log performance monitor every 50 values
+    if (performanceMonitor.total.values.length % 50 === 0) {
+      console.log('Performance monitor:', performanceMonitor)
+    }
+
+    if (performanceMonitor.total.values.length > 50) {
+      performanceMonitor.total.values = []
+    }
+    if (performanceMonitor.getFrame.values.length > 50) {
+      performanceMonitor.getFrame.values = []
+    }
+    if (performanceMonitor.inferCorners.values.length > 50) {
+      performanceMonitor.inferCorners.values = []
+    }
+    if (performanceMonitor.postprocessImage.values.length > 50) {
+      performanceMonitor.postprocessImage.values = []
+    }
 
     setTimeout(() => requestAnimationFrame(scannerLoop), delay)
   }
